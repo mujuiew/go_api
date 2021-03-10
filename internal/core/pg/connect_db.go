1933691ae1025c4bd843347445684da5e63ff657
benjawan.ru <benjawan.ru@tnis.com>
2021-03-10 21:50:39 +0700

    [feat] commit2

---
+++ internal/core/pg/connect_db.go
@@ -0,0 +1,47 @@
+package pg
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "postgres"
+	dbname   = "payment"
+)
+
+func InitDB() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected!")
+}
+func homePage(w http.ResponseWriter, r *http.Request) {
+}
+func handle() {
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":8001", nil))
+}
+
+func main() {
+	InitDB()
+	handle()
+}
